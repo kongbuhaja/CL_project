7f65141d725ff945531a9e22febe74765842e2da
kongbuhaja <pcar530@gmail.com>
2023-12-03 18:27:52 +0900

    update dockerfile

--- train.sh
+++ train.sh
@@ -3,10 +3,10 @@
 # python3 train.py --model VGG19 --channel 16 --epochs 50
 # python3 train.py --model GoogleNet22 --channel 16 --epochs 50
 
-python3 train.py --model DarkNet19 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100
-python3 train.py --model ResNet18 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100
-python3 train.py --model VGG19 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100
-python3 train.py --model GoogleNet22 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100
+python3 train.py --model DarkNet19 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 --init_lr 0.1
+python3 train.py --model ResNet18 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 --init_lr 0.1
+python3 train.py --model VGG19 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 --init_lr 0.1
+python3 train.py --model GoogleNet22 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 --init_lr 0.1
 
 # python3 train.py --model DarkNet19 --channel 16 --epochs 200 --eval_term 10 --dataset imagenet
 # python3 train.py --model ResNet18 --channel 16 --epochs 200 --eval_term 10 --dataset imagenet
