cc2309018b1fd2aab85b9aea972c6106130af6b1
kongbuhaja <pcar530@gmail.com>
2023-12-07 03:13:51 +0900

    update train.sh

--- multi_model_train.sh
+++ multi_model_train.sh
@@ -1,5 +1,5 @@
-nohup python3 train.py --model DarkNet19 --channel 64 --epochs 200 --eval_term 5 --dataset mnist --batch_size 150 --gpus 0 > DarkNet19.log 2>&1 &
-nohup python3 train.py --model ResNet18 --channel 64 --epochs 200 --eval_term 5 --dataset mnist --batch_size 150 --gpus 1 > ResNet18.log 2>&1 &
-nohup python3 train.py --model VGG19 --channel 64 --epochs 200 --eval_term 5 --dataset mnist --batch_size 150 --gpus 2 > VGG19.log 2>&1 &
-nohup python3 train.py --model GoogleNet22 --channel 64 --epochs 200 --eval_term 5 --dataset mnist --batch_size 150 --gpus 3 > GoogleNet22.log 2>&1 &
+nohup python3 train.py --model DarkNet19 --gpus 0 --channel 64 --epochs 200 --eval_term 5 --dataset mnist --batch_size 150  > DarkNet19.log 2>&1 &
+nohup python3 train.py --model ResNet18 --gpus 1 --channel 64 --epochs 200 --eval_term 5 --dataset mnist --batch_size 150 > ResNet18.log 2>&1 &
+nohup python3 train.py --model VGG19 --gpus 2 --channel 64 --epochs 200 --eval_term 5 --dataset mnist --batch_size 150 > VGG19.log 2>&1 &
+nohup python3 train.py --model GoogleNet22 --gpus 3 --channel 64 --epochs 200 --eval_term 5 --dataset mnist --batch_size 150 > GoogleNet22.log 2>&1 &
 
--- train.sh
+++ train.sh
@@ -1,19 +1,21 @@
-python3 train.py --model DarkNet19 --channel 16 --epochs 100
-python3 train.py --model ResNet18 --channel 16 --epochs 100
-python3 train.py --model VGG19 --channel 16 --epochs 100
-python3 train.py --model GoogleNet22 --channel 16 --epochs 100
+# python3 train.py --model DarkNet19 --channel 16 --epochs 100
+# python3 train.py --model ResNet18 --channel 16 --epochs 100
+# python3 train.py --model VGG19 --channel 16 --epochs 100
+# python3 train.py --model GoogleNet22 --channel 16 --epochs 100
 
-python3 train.py --model DarkNet19 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 
-python3 train.py --model ResNet18 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 
-python3 train.py --model VGG19 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 
-python3 train.py --model GoogleNet22 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 
+# python3 train.py --model DarkNet19 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 
+# python3 train.py --model ResNet18 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 
+# python3 train.py --model VGG19 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 
+# python3 train.py --model GoogleNet22 --channel 16 --epochs 200 --eval_term 5 --dataset cifar100 
  
-# python3 train.py --model DarkNet19 --channel 64 --epochs 100 --eval_term 10 --dataset cifar100 --batch_size 100 
-# python3 train.py --model ResNet18 --channel 64 --epochs 100 --eval_term 10 --dataset cifar100 --batch_size 100
-# python3 train.py --model VGG19 --channel 64 --epochs 100 --eval_term 10 --dataset cifar100 --batch_size 100
-# python3 train.py --model GoogleNet22 --channel 64 --epochs 100 --eval_term 10 --dataset cifar100 --batch_size 100
+python3 train.py --model DarkNet19 --channel 64 --epochs 200 --eval_term 5 --dataset cifar100 --batch_size 100 
+# python3 train.py --model ResNet18 --channel 64 --epochs 200 --eval_term 5 --dataset cifar100 --batch_size 100
+python3 train.py --model VGG19 --channel 64 --epochs 200 --eval_term 5 --dataset cifar100 --batch_size 100
+# python3 train.py --model GoogleNet22 --channel 64 --epochs 200 --eval_term 5 --dataset cifar100 --batch_size 100
+python3 train.py --model ResNet18 --channel 64 --epochs 200 --eval_term 5 --dataset cifar100 --batch_size 100
+python3 train.py --model GoogleNet22 --channel 64 --epochs 200 --eval_term 5 --dataset cifar100 --batch_size 100
 
-# python3 train.py --model DarkNet19 --gpus 0 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
-# python3 train.py --model ResNet18 --gpus 1 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
-# python3 train.py --model VGG19 --gpus 2 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
-# python3 train.py --model GoogleNet22 --gpus 3 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
+# python3 train.py --model DarkNet19 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
+# python3 train.py --model ResNet18 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
+# python3 train.py --model VGG19 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
+# python3 train.py --model GoogleNet22 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
