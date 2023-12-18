78a5a9a7108941875f1bf60f732728633f22f40b
kongbuhaja <pcar530@gmail.com>
2023-12-19 00:30:40 +0900

    update task.sh

--- task.sh
+++ task.sh
@@ -18,19 +18,19 @@
 # python3 train.py --model VGG19 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
 # python3 train.py --model GoogleNet22 --channel 64 --epochs 500 --eval_term 5 --dataset imagenet --batch_size 100
 start_background_task() {
-    nohup python3 "$1.py --model $2 --channel 64 --eval_term 5 --dataset $3 --optimizer SGD > $2.log" 2>&1 &
+    nohup python3 "$1".py --model "$2" --channel 64 --eval_term 5 --dataset "$3" --optimizer SGD > "$2.log" 2>&1 &
 }
 
 # 대기 함수
 wait_for_completion() {
     while pgrep -f "python3 $1.py --model $2 --channel 64 --eval_term 5 --dataset $3 --optimizer SGD" > /dev/null; do
-        sleep 1
+        sleep 5
     done
 }
 
 task() {
-    start_background_task "$1 $2 $3 $4"
-    wait_for_completion "$1 $2 $3 $4"
+    start_background_task "$1" "$2" "$3"
+    wait_for_completion "$1" "$2" "$3"
 }
 
-task train SGD mnist
+task train ResNet18 mnist
